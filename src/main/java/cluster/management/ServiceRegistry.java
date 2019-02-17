14c1e98754455e2cdcf84d8c4312094f5a42c5ef
Michael Pogrebinsky <smichpog2@gmail.com>
2019-02-17 10:39:38 -0800

    createServiceRegistryZnode

---
+++ src/main/java/cluster/management/ServiceRegistry.java
@@ -0,0 +1,27 @@
+package cluster.management;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ServiceRegistry {
+    private static final String REGISTRY_ZNODE = "/service_registry";
+    private final ZooKeeper zooKeeper;
+
+    public ServiceRegistry(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
+    }
+
+    private void createServiceRegistryZnode() {
+        try {
+            if (zooKeeper.exists(REGISTRY_ZNODE, false) == null) {
+                zooKeeper.create(REGISTRY_ZNODE, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
