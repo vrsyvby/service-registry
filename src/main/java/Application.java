7a5b51c6deb9b5100da519c89295e9529ef7b7b1
Michael Pogrebinsky <smichpog2@gmail.com>
2019-02-17 10:00:32 -0800

    Start

---
+++ src/main/java/Application.java
@@ -0,0 +1,64 @@
+import cluster.management.LeaderElection;
+import cluster.management.ServiceRegistry;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class Application implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        int serverPort = 8080;
+        if (args.length == 1) {
+            serverPort = Integer.parseInt(args[0]);
+        }
+        Application application = new Application();
+        ZooKeeper zooKeeper = application.connectToZookeeper();
+
+        ServiceRegistry serviceRegistry = new ServiceRegistry(zooKeeper);
+        ParallelSearch parallelSearch = new ParallelSearch(zooKeeper, serviceRegistry, serverPort);
+
+        LeaderElection leaderElection = new LeaderElection(zooKeeper, parallelSearch);
+        leaderElection.volunteerForLeadership();
+        leaderElection.reelectLeader();
+
+        application.run();
+        application.close();
+        System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public ZooKeeper connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+        return zooKeeper;
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from Zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+        }
+    }
+}
